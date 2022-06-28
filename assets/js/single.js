ea603062e7bc1820a7d1959228680619adf1e1db
kea6t <keasay26@gmail.com>
2022-06-28 15:52:42 -0400

    add an extra message

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -9,6 +10,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function (data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated Issues
+                if(response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request");
@@ -50,5 +56,19 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var displayWarning = function(repo) {
+    // add text container to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See more issues on GitHub";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
 
 getRepoIssues("facebook/react");
